e5f6de6a2d8e4efe5cd7e54e1214984bf8665b89
Angel Iván Lozano Ramírez <angel_ivan_5@outlook.com>
2021-07-02 22:43:08 -0500

    Create headset-serial.ino
    Send data through software serial port (Arduino UNO)

---
+++ headset-serial/headset-serial.ino
@@ -0,0 +1,127 @@
+////////////////////////////////////////////////////////////////////////
+// Arduino Bluetooth Interface with Mindwave
+// 
+// This is example code provided by NeuroSky, Inc. and is provided
+// license free.
+//
+// This modification allows view data trough serial monitor
+// Lozano Ramirez Angel Ivan
+// Mexico  2.07.2021
+////////////////////////////////////////////////////////////////////////
+
+#include <SoftwareSerial.h>
+SoftwareSerial BT(4,2); //Rx/Tx
+
+#define LED 13
+#define BAUDRATE 57600
+#define DEBUGOUTPUT 0
+
+#define powercontrol 10
+
+// checksum variables
+byte  generatedChecksum = 0;
+byte  checksum = 0; 
+int   payloadLength = 0;
+byte  payloadData[64] = {0};
+byte  poorQuality = 0;
+byte  attention = 0;
+byte  meditation = 0;
+
+// system variables
+long    lastReceivedPacket = 0;
+boolean bigPacket = false;
+
+//////////////////////////
+// Microprocessor Setup //
+//////////////////////////
+void setup(){
+  pinMode(LED, OUTPUT);
+  BT.begin(BAUDRATE);           // Software serial port  (ATMEGA328P)
+  Serial.begin(BAUDRATE);           // USB
+}
+
+////////////////////////////////
+// Read data from Serial UART //
+////////////////////////////////
+byte ReadOneByte() {
+  int ByteRead;
+  while(!BT.available());
+  ByteRead = BT.read();
+  
+  #if DEBUGOUTPUT  
+    Serial.print((char)ByteRead);   // echo the same byte out the USB serial (for debug purposes)
+  #endif
+  
+  return ByteRead;
+}
+
+/////////////
+//MAIN LOOP//
+/////////////
+void loop() {
+  // Look for sync bytes
+  if(ReadOneByte() == 170) {
+    if(ReadOneByte() == 170) {
+      payloadLength = ReadOneByte();
+      if(payloadLength > 169)                      //Payload length can not be greater than 169
+      return;
+
+      generatedChecksum = 0;        
+      for(int i = 0; i < payloadLength; i++) {  
+        payloadData[i] = ReadOneByte();            //Read payload into memory
+        generatedChecksum += payloadData[i];
+      }   
+
+      checksum = ReadOneByte();                      //Read checksum byte from stream      
+      generatedChecksum = 255 - generatedChecksum;   //Take one's compliment of generated checksum
+
+        if(checksum == generatedChecksum) {    
+
+        poorQuality = 200;
+        attention = 0;
+        meditation = 0;
+
+        for(int i = 0; i < payloadLength; i++) {    // Parse the payload
+          switch (payloadData[i]) {
+          case 2:
+            i++;            
+            poorQuality = payloadData[i];
+            bigPacket = true;            
+            break;
+          case 4:
+            i++;
+            attention = payloadData[i];                        
+            break;
+          case 5:
+            i++;
+            meditation = payloadData[i];
+            break;
+          case 0x80:
+            i = i + 3;
+            break;
+          case 0x83:
+            i = i + 25;      
+            break;
+          default:
+            break;
+          } // switch
+        } // for loop
+
+#if !DEBUGOUTPUT
+        // *** Add your code here ***
+        if(bigPacket) {
+          if(poorQuality == 0)  digitalWrite(LED, HIGH);
+          else  digitalWrite(LED, LOW);
+          Serial.print("Attention: ");
+          Serial.print(attention);
+          Serial.print("\n");
+        }
+#endif        
+        bigPacket = false;        
+      }
+      else {
+        // Checksum Error
+      }  // end if else for checksum
+    } // end if read 0xAA byte
+  } // end if read 0xAA byte
+}
